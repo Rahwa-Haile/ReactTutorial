7b2490cf58c9d79bcc6083f31b9c95115a4322ba
Rahwa Yohannes <rahwahaile578@gmail.com>
2023-01-23 18:39:23 +0300

    Nested components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import  ReactDOM  from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 function Greeting() {
-  return <h1>My name is Rahwa and this is my first react component</h1>
+  return (
+    <div>
+      <Person/>
+      <Message/>
+    </div>
+  );
 }
 
-ReactDOM.render(<Greeting/>, document.getElementById('root'))
+const Person = ()=>(
+  <h1>Rahwa Yohannes</h1>
+)
+const Message = () => <p>This is my message</p>;
+// const Greeting = ()=>{
+//   return React.createElement('h1', {}, 'Hello World')
+// }
+// const Greeting = () => {
+//   return React.createElement("div", {}, React.createElement('h1', {}, 'Hello World'));
+// };
+ReactDOM.render(<Greeting />, document.getElementById("root"));
