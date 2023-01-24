f473b20943e369f167b4bfa39509092676df6ea1
Rahwa Yohannes <rahwahaile578@gmail.com>
2023-01-24 15:40:50 +0300

    props

--- src/index.js
+++ src/index.js
@@ -2,36 +2,44 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const firstJob = {
+  author: "by Prince Harry The Duke of Sussex and Random House Audio",
+  title: "Spare",
+  img: "https://m.media-amazon.com/images/I/91NlZifnNEL._AC_UL480_FMwebp_QL65_.jpg"
+};
+const secondJob = {
+  author: "Mark Manson",
+  title: "The Subtle Art of Not Giving a F*ck",
+  img: "https://images-na.ssl-images-amazon.com/images/I/71QKQ9mwV7L._AC_UL900_SR900,600_.jpg",
+};
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author={firstJob.author}
+        title={firstJob.title}
+        img={firstJob.img}
+      />
+      <Book
+        author={secondJob.author}
+        title={secondJob.title}
+        img={secondJob.img}
+      />
     </section>
   );
 }
 
-const author = "by Prince Harry The Duke of Sussex and Random House Audio";
-const Book = () => {
-  const title = "Spare";
+const Book = (props) => {
+  console.log(props)
   return (
     <article className="book">
       <img
-        src="https://m.media-amazon.com/images/I/91NlZifnNEL._AC_UL480_FMwebp_QL65_.jpg"
+        src={props.img}
         alt=""
       />
-      <h1>{title}</h1>
-      <h5>{author}</h5>
+      <h1>{props.title}</h1>
+      <h5>{props.author.toUpperCase()}</h5>
     </article>
   );
 };
