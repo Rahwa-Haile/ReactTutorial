3ba94f5fe98263ad8183c56e3caf350f68cf1df7
Rahwa Yohannes <rahwahaile578@gmail.com>
2023-01-25 18:38:29 +0300

    Key Prop And Spread Operator

--- src/properList.js
+++ src/properList.js
@@ -4,13 +4,11 @@ import "./index.css";
 
 const books = [
   {
-    id: 1,
     author: "by Prince Harry The Duke of Sussex and Random House Audio",
     title: "Spare",
     img: "https://m.media-amazon.com/images/I/91NlZifnNEL._AC_UL480_FMwebp_QL65_.jpg",
   },
   {
-    id: 2,
     author: "Mark Manson",
     title: "The Subtle Art of Not Giving a F*ck",
     img: "https://images-na.ssl-images-amazon.com/images/I/71QKQ9mwV7L._AC_UL900_SR900,600_.jpg",
@@ -21,13 +19,14 @@ function BookList() {
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book key={books.id} {...book} />;
+        return <Book book={book} />;
       })}
     </section>
   );
 }
 
-const Book = ({ img, author, title }) => {
+const Book = (props) => {
+  const { img, author, title } = props.book;
   return (
     <article className="book">
       <img src={img} alt="" />
